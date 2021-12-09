38105d33d5ac83e37c89688f3be6169e2bd38252
Maciej Lisowski <macieejl00@gmail.com>
2021-12-09 18:40:46 +0100

    First commit
    Signed-off-by: Maciej Lisowski <macieejl00@gmail.com>

---
+++ dbprojekt/src/main/java/pl/mlisowski/dbprojekt/DbprojektApplication.java
@@ -0,0 +1,13 @@
+package pl.mlisowski.dbprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbprojektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbprojektApplication.class, args);
+	}
+
+}
---
+++ dbprojekt/src/test/java/pl/mlisowski/dbprojekt/DbprojektApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.mlisowski.dbprojekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbprojektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
